c029a5b5e6297f8df73701ad922357b411bbc5c9
Abdus Salam <salaam.mbstu@gmail.com>
2021-04-06 17:04:39 +0600

    Updated categories page

--- resources/views/backend/view-categories.blade.php
+++ resources/views/backend/view-categories.blade.php
@@ -23,6 +23,9 @@
                                   <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                     Category Slug
                                   </th>
+                                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Action
+                                  </th>
                                 </tr>
                               </thead>
                               <tbody class="bg-white divide-y divide-gray-200">
@@ -30,9 +33,6 @@
                                     <tr>
                                       <td class="px-6 py-4 whitespace-nowrap">
                                         <div class="flex items-center">
-                                          <div class="flex-shrink-0 h-10 w-10">
-                                            <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt="">
-                                          </div>
                                           <div class="ml-4">
                                             <div class="text-sm font-medium text-gray-900">
                                               {{ $category->name }}
@@ -47,8 +47,17 @@
                                             </div>
                                           </div>
                                       </td>
-                                      <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                        <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                      <td class="px-6 py-4 whitespace-nowrap">
+                                        <div class="ml-4">
+                                            <div class="text-sm font-medium text-gray-900">
+                                              <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded">
+                                                <a href="#">Edit</a>
+                                              </button>
+                                              <button class="bg-transparent hover:bg-red-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded">
+                                                <a href="#">Delete</a>
+                                              </button>
+                                            </div>
+                                          </div>
                                       </td>
                                     </tr>
                                 @endforeach
