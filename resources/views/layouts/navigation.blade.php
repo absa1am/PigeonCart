021c14becf71ba6245688400d7d051ba69eb2cf8
Abdus Salam <salaam.mbstu@gmail.com>
2021-08-17 17:07:04 +0600

    Updated navigation bar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -121,7 +121,7 @@
                         <x-dropdown align="right" width="48">
                             <x-slot name="trigger">
                                 <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                                    <div>Orders</div>
+                                    <div>Orders(<span class="text-green-500">{{ count(App\Models\Order::where('status', 'Pending')->get()) }}</span>)</div>
 
                                     <div class="ml-1">
                                         <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -181,7 +181,7 @@
                     <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                         <x-nav-link href="{{ route('view-contacts') }}">
                             {{ __('Messages') }}
-                            ({{ count(\App\Models\Contact::where('status', 'Unread')->get()) }})
+                            (<span class="text-green-500">{{ count(\App\Models\Contact::where('status', 'Unread')->get()) }}</span>)
                         </x-nav-link>
                     </div>
                 </div>
