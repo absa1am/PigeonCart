8068fbe167a61adfe222c157a8494bcac9d09f67
Abdus Salam <salaam.mbstu@gmail.com>
2021-08-28 19:15:32 +0600

    Updated database table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,10 +18,10 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('city');
-            $table->string('zip');
-            $table->string('address');
-            $table->string('phone');
+            $table->string('city')->nullable();
+            $table->string('zip')->nullable();
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
