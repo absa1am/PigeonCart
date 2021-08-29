989dd00f8e8b3fc2c01b3ea1dd5fb7c395f0b55c
Abdus Salam <salaam.mbstu@gmail.com>
2021-08-29 19:09:15 +0600

    Added payment method

--- resources/views/frontend/checkout.blade.php
+++ resources/views/frontend/checkout.blade.php
@@ -20,7 +20,17 @@
                             <div class="mt-6">
                                 <button class="flex items-center justify-between w-full bg-white rounded-md border-2 border-blue-500 p-4 focus:outline-none">
                                     <label class="flex items-center">
-                                        <input type="radio" class="form-radio h-5 w-5 text-blue-600" checked><span class="ml-2 text-sm text-gray-700">Cash On Delivery</span>
+                                        <input type="radio" class="form-radio h-5 w-5 text-blue-600"><span class="ml-2 text-sm text-gray-700">Cash On Delivery</span>
+                                    </label>
+
+                                    <span class="text-gray-600 text-sm">50 Tk</span>
+                                </button>
+                            </div>
+
+                            <div class="mt-6">
+                                <button class="flex items-center justify-between w-full bg-white rounded-md border-2 border-blue-500 p-4 focus:outline-none">
+                                    <label class="flex items-center">
+                                        <input type="radio" class="form-radio h-5 w-5 text-blue-600"><span class="ml-2 text-sm text-gray-700">Cash on bKash</span>
                                     </label>
 
                                     <span class="text-gray-600 text-sm">50 Tk</span>
